a5f4b14c13b5a2040fdabcb604100e1b6f008c4b
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2019-05-19 16:59:13 -0400

    Functionality

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(bodyParser.urlencoded({
 app.get('/', function (req, res) {
     res.render('index', {
         newTitle: null,
-        titleNumber: null
+        titleNumber: 0
     });
 })
 app.post('/', function (req, res) {
 
-    const prep = ['a', 'aboard', 'about', 'above', 'across', 'after', 'against', 'along', 'amid', 'among', 'an', 'and', 'anti', 'around', 'as', 'at', 'before', 'behind', 'below', 'beneath', 'beside', 'besides', 'between', 'beyond', 'but', 'by', 'ca', 'concerning', 'considering', 'de', 'despite', 'down', 'during', 'except', 'excepting', 'excluding', 'following', 'for', 'from', 'in', 'inside', 'into', 'like', 'minus', 'near', 'of', 'off', 'nor', 'on', 'onto', 'opposite', 'or', 'outside', 'over', 'past', 'per', 'plus', 'regarding', 'round', 'save', 'since', 'than', 'the', 'this', 'through', 'to', 'toward', 'towards', 'under', 'underneath', 'unlike', 'until', 'up', 'upon', 'versus', 'via', 'von', 'with', 'within', 'without'];
+    const prep = ['a', 'aboard', 'about', 'above', 'across', 'after', 'against', 'along', 'amid', 'among', 'an', 'and', 'anti', 'around', 'as', 'at', 'before', 'behind', 'below', 'beneath', 'beside', 'besides', 'between', 'beyond', 'but', 'by', 'c.', 'ca.', 'concerning', 'considering', 'de', 'despite', 'down', 'during', 'except', 'excepting', 'excluding', 'following', 'for', 'from', 'in', 'inside', 'into', 'like', 'minus', 'near', 'of', 'off', 'nor', 'on', 'onto', 'opposite', 'or', 'outside', 'over', 'past', 'per', 'plus', 'regarding', 'round', 'save', 'since', 'than', 'the', 'this', 'through', 'to', 'toward', 'towards', 'under', 'underneath', 'unlike', 'until', 'up', 'upon', 'versus', 'via', 'von', 'with', 'within', 'without'];
 
     // if (/\r\n/.test(req.body.title) === true) {
     //     console.log("This is multiple titles");
@@ -34,14 +34,14 @@ app.post('/', function (req, res) {
     //     console.log(testA);
     // })
 
-    console.log(req.body.title);
+    console.log(req.body);
     let titleArray = req.body.title.toLowerCase().split(/\r\n/);
     // .split(/\r\n/);
 
     // titleNum = titleArray.length;
     // titleNum = titleArray.length;
     let titleNum = titleArray.length;
-    console.log(titleNum);
+   
     let wordArray = [];
 
     titleArray.forEach(element => {
