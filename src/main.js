da7856a49471f89a1d50df19a1773100ca3ae99b
Ryan Feigenbaum <48868107+royalfig@users.noreply.github.com>
2023-05-10 17:45:56 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Toasted from "vue-toasted";
-import VueAnalytics from "vue-analytics";
+
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
-
-Vue.use(VueAnalytics, {
-  id: "UA-136381710-2"
-});
 
 Vue.use(Toasted, {
   duration: 4000,
